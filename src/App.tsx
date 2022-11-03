a22368fb778e702e54398e2b6b90795e0e32803a
Evan Hamer <112445690+Roundabout01@users.noreply.github.com>
2022-11-02 23:33:39 -0400

    HTML testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import React from "react";
 import "./App.css";
+import { Button } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript: Evan Hamer...
+                UD CISC275 with React Hooks and TypeScript: COS420 Evan Hamer...
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
+                automatically reload. Hello World
+                <span style={{ color: "red" }}>col</span>
             </p>
+            <ul>
+                <li>First</li>
+                <li>Second</li>
+                <li>
+                    <img src="src/background.jpg" alt="test" />
+                </li>
+            </ul>
+            <Button onClick={() => console.log("I am logged")}>Click Me</Button>
         </div>
     );
 }
